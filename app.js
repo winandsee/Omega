5bd7a08630253a8069c42c56aacdaa84cfcaf565
winandsee <69314632+winandsee@users.noreply.github.com>
2020-08-06 20:38:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 
 ///Edit Under This
 const firebaseConfig = {
-  apiKey: "AIzaSyB_sayvKHvV_s2UNdLm8xVnXJ7GKQL0qko",
-  authDomain: "zee-hack.firebaseapp.com",
-  databaseURL: "https://zee-hack.firebaseio.com",
-  projectId: "zee-hack",
-  storageBucket: "zee-hack.appspot.com",
-  messagingSenderId: "993676200693",
-  appId: "1:993676200693:web:c5059d76b19e9fcb30d3bc",
-  measurementId: "G-X4CR2T5LTB"
+  apiKey: "AIzaSyDdpXJAAkikF6_PScNO0Ug8o1EB1G6cmtU",
+  authDomain: "hacking-bd768.firebaseapp.com",
+  databaseURL: "https://hacking-bd768.firebaseio.com",
+  projectId: "hacking-bd768",
+  storageBucket: "hacking-bd768.appspot.com",
+  messagingSenderId: "184530741081",
+  appId: "1:184530741081:web:ad6480b08bb02a3e4d0e48",
+  measurementId: "G-PEY0XY0ZTT"
 };
 ////Do Not Edit Below This
 
