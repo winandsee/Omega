cffac6838177cc3b5dcd25735f5e828f9480f2fd
winandsee <69314632+winandsee@users.noreply.github.com>
2020-08-06 20:18:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,16 @@
 
 
 ///Edit Under This
-
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB_sayvKHvV_s2UNdLm8xVnXJ7GKQL0qko",
+  authDomain: "zee-hack.firebaseapp.com",
+  databaseURL: "https://zee-hack.firebaseio.com",
+  projectId: "zee-hack",
+  storageBucket: "zee-hack.appspot.com",
+  messagingSenderId: "993676200693",
+  appId: "1:993676200693:web:c5059d76b19e9fcb30d3bc",
+  measurementId: "G-X4CR2T5LTB"
 };
-
-     
- 
 ////Do Not Edit Below This
 
 firebase.initializeApp(firebaseConfig);
