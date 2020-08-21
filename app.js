23a7d8ee9629aed7c7e665d50d264d1ef11c754c
winandsee <69314632+winandsee@users.noreply.github.com>
2020-08-22 01:52:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,16 @@
 
 
 ///Edit Under This only
-
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBJKu-usz4U24d1r1PIT5zTUM5VyH1aBv0",
+  authDomain: "new-awesome-b5fc1.firebaseapp.com",
+  databaseURL: "https://new-awesome-b5fc1.firebaseio.com",
+  projectId: "new-awesome-b5fc1",
+  storageBucket: "new-awesome-b5fc1.appspot.com",
+  messagingSenderId: "165959613593",
+  appId: "1:165959613593:web:a130763cbb0efc592d0c08",
+  measurementId: "G-QV2GLQVE8R"
 };
-
-     
- 
 ////Do Not Edit Below This
 
 firebase.initializeApp(firebaseConfig);
